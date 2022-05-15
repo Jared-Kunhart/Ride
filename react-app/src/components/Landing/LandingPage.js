be066fcf042697b294262fb6f71456656144d86d
Jared Kunhart <hakujaden@comcast.net>
2022-05-15 04:22:54 -0500

    stylin login/landing done

--- react-app/src/components/Landing/LandingPage.js
+++ react-app/src/components/Landing/LandingPage.js
@@ -1,4 +1,10 @@
+import { Pagination, Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react'
 import { Link } from 'react-router-dom';
+import 'swiper/swiper.min.css'
+import 'swiper/modules/pagination/pagination.min.css'
+import 'swiper/modules/navigation/navigation.min.css'
+import "./styles.css";
 import './landing.css'
 
 export default function LandingPage() {
@@ -26,7 +32,60 @@ export default function LandingPage() {
                     </div>
                 </div>
                 <div id='sub_page_content'>
-
+                <div id='sub_page_carousel'>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={60}
+                    navigation={true}
+                    height={"500px"}
+                    autoplay={false}
+                    loop={true}
+                    pagination={{
+                    clickable: true,
+                    }}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                    <div><div className='slider_pics'>
+                    <img alt='' id='slider_jfif' src='/static/images/steve.jfif'/></div>
+                    “As a student, it's hard to complete my class work around a schedule. I started driving more with Lyft and realized it was the perfect opportunity to make money and work on my own time! Driving with Lyft gives me freedom in my schedule to focus on school, which is my main priority.” {"\n"}
+                    {"\n"}
+                     — Steve
+                    {"\n"}
+                    Driving with Lyft Since 1984.
+                    </div>
+                    </SwiperSlide>
+                    <SwiperSlide><div><div className='slider_pics'>
+                    <img alt='' id='slider_jfif' src='/static/images/damian.jfif'/></div>
+                    “My motto is very simple. It doesn't cost you a penny to be nice and kind, but it will cost you everything if you're not. If I'm free and somebody needs my help, I'll be the first one to jump in, in a heartbeat.” {"\n"}
+                    {"\n"}
+                    — Damian
+                    {"\n"}
+                    Driving with Ride since 1954.
+                    </div>
+                    </SwiperSlide>
+                    <SwiperSlide><div><div className='slider_pics'>
+                    <img alt='' id='slider_jfif' src='/static/images/roger.jfif'/></div>
+                    “Driving with Lyft is the perfect way to make money and be there for my family's needs. I love providing a way to get my passengers from point A to B. Independence is key for me, and I enjoy meeting new people while driving!”{"\n"}
+                    {"\n"}
+                    — Roger
+                    {"\n"}
+                     Driving with Ride since WW2.
+                     </div>
+                     </SwiperSlide>
+                    <SwiperSlide><div><div className='slider_pics'>
+                    <img alt='' id='slider_jfif' src='/static/images/jason.jfif'/></div>
+                    "I got to the drop-off address, and he said it wasn't where he lived. He couldn't communicate an address because he was so drunk. He told me to turn on certain streets, but we were going in circles. We approached a liquor store, and he told me to stop so he could go in. He stayed in the car for five minutes with his hand in his pocket and told me I was going to be a good boy and wait for him. Then he threatened me and left his food in the car."
+                    {"\n"}
+                    {"\n"}
+                    — Jason
+                    {"\n"}
+                    Driving with Ride since 2014.
+                    </div>
+                    </SwiperSlide>
+                </Swiper>
+                </div>
                 </div>
             </div>
         </div>
