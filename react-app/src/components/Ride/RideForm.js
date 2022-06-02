0cfa528886bec29f544e8245224903da492bd003
Jared Kunhart <hakujaden@comcast.net>
2022-06-02 01:35:54 -0500

    autocomplete and streetview

--- react-app/src/components/Ride/RideForm.js
+++ react-app/src/components/Ride/RideForm.js
@@ -1,4 +1,5 @@
 import Geocode from 'react-geocode'
+import { Autocomplete } from '@react-google-maps/api';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createBooking } from '../../store/bookings';
@@ -86,41 +87,53 @@ const RideForm = () => {
     Geocode.setLocationType("ROOFTOP");
     Geocode.enableDebug();
 
-    
+    const onOriginAutoComplete = (e) => {
+        setOriginAddress(e)
+    }
+
+    const onDestinationAutoComplete = (e) => {
+        setDestinationAddress(e)
+    }
 
     return (
         <>
         <div id='form_header'><h1>Where to {user.firstname} {user.lastname} ?</h1></div>
           <form onSubmit={(e)=>handleSubmit(e)}>
-                  <input
-                    id='origin_input'
-                    type='text'
-                    value={originAddress}
-                    placeholder="Enter a pickup location"
-                    onChange={(e)=>setOriginAddress(e.target.value)} />
-                  <input
-                   type='text'
-                   id='destination_input'
-                   value={destinationAddress}
-                   placeholder="Where are you going ?"
-                   onChange={(e)=>setDestinationAddress(e.target.value)} />
-                <img alt='' id='ride_pickup_logo' src='/static/images/ridepickup.png' />
-                <img alt='' id='ride_dropoff_logo' src='/static/images/ridedropoff.png' />
-                <div id='future_auto_complete_div'>
-                <div id='error_div_post'>
-                    {errors.map((error, ind) => (
-                    <div key={ind}>{error}</div>
-                    ))}
-                </div>
-              <button
-              id='ride_submit_button'
-              type="submit"
-              disabled={originAddress && destinationAddress ? false : true}
-              >
-                  Ride
-              </button>
-              </div>
-          </form>
+            <Autocomplete>
+                <input
+                id='origin_input'
+                type='text'
+                value={originAddress}
+                placeholder="Enter a pickup location"
+                onBlur={(e) => onOriginAutoComplete(e.target.value)}
+                onChange={(e) => setOriginAddress(e.target.value)} />
+            </Autocomplete>
+            <Autocomplete>
+                <input
+                type='text'
+                id='destination_input'
+                value={destinationAddress}
+                placeholder="Where are you going ?"
+                onBlur={(e) => onDestinationAutoComplete(e.target.value)}
+                onChange={(e) => setDestinationAddress(e.target.value)} />
+            </Autocomplete>
+            <img alt='' id='ride_pickup_logo' src='/static/images/ridepickup.png' />
+            <img alt='' id='ride_dropoff_logo' src='/static/images/ridedropoff.png' />
+            <div id='future_auto_complete_div'>
+            <div id='error_div_post'>
+                {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+                ))}
+            </div>
+            <button
+            id='ride_submit_button'
+            type="submit"
+            disabled={originAddress && destinationAddress ? false : true}
+            >
+                Ride
+            </button>
+            </div>
+        </form>
         </>
     )
 }
