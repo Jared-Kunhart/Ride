9f23a61783f1cf74bb29b912047ca6f191580678
Jared Kunhart <Jared-Kunhart@proton.me>
2022-12-05 21:27:32 -0600

    vercel

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, db.Enum, primary_key=True)
+    id = db.Column(db.Enum, primary_key=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     firstname = db.Column(db.String(40), nullable=False)
     lastname = db.Column(db.String(40), nullable=False)
