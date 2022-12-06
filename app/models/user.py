9e3d86b26af8f25f1e714a7111fbf17c06a7651b
Jared Kunhart <Jared-Kunhart@proton.me>
2022-12-05 21:32:14 -0600

    vercel

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,13 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Identity
 from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Enum, primary_key=True)
+    id = db.Column(db.Integer, Identity(start=1, cycle=True), primary_key=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     firstname = db.Column(db.String(40), nullable=False)
     lastname = db.Column(db.String(40), nullable=False)
