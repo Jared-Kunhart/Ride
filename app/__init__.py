d9c0ed78e0dadb5a9612e047eea899b60c25d0b0
Jared Kunhart <Jared-Kunhart@proton.me>
2022-12-06 15:43:03 -0600

    vercel

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from sqlalchemy.dialects import registry
-from sqlalchemy import create_engine
+# from sqlalchemy.dialects import registry
+# from sqlalchemy import create_engine
 
 from .models import db, User
 from .api.user_routes import user_routes
@@ -24,8 +24,8 @@ app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 login = LoginManager(app)
 login.login_view = 'auth.unauthorized'
 
-engine = create_engine(os.environ["DATABASE_URL"])
-registry.register("cockroachdb", "cockroachdb.sqlalchemy.dialect", "CockroachDBDialect")
+# engine = create_engine(os.environ["DATABASE_URL"])
+# registry.register("cockroachdb", "cockroachdb.sqlalchemy.dialect", "CockroachDBDialect")
 
 @login.user_loader
 def load_user(id):
