470e6de0c7a5e4a7f41d6c0c09c9f08fc4c91973
Jared Kunhart <Jared-Kunhart@proton.me>
2022-12-08 13:19:44 -0600

    vercel

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -36,7 +36,7 @@ def seed_users():
     # db.session.add(passenger_3)
     # db.session.add(driver_3)
 
-    db.session.commit()
+    # db.session.commit()
 
 
 # Uses a raw SQL query to TRUNCATE the users table.
